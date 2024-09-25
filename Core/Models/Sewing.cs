8755b5087d1ff4dc56fca0efef9fea7d68840114
jasstitche <jaspstitches@gmail.com>
2024-09-25 11:03:59 +0100

    last commit

---
+++ Core/Models/Sewing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Sewing
+    {
+        public int SewingId { get; set; } 
+        public string ProductName { get; set; } 
+        public string Category { get; set; } 
+        public decimal BasePrice { get; set; } 
+        public List<string> SizeOptions { get; set; } 
+        public int TurnaroundTimeInDays { get; set; } 
+
+    
+    }
+}
+
